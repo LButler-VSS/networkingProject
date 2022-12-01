2dcd9c730f79ab2a29f25575bdb5215962ef51dc
Logan Butler <logan3528@gmail.com>
2022-11-30 20:21:53 -0700

    server respone added

--- server/server.py
+++ server/server.py
@@ -24,9 +24,10 @@ filename, filesize = received.split(SEPARATOR)
 filename = os.path.basename(filename)
 filesize = int(filesize)
 
-
+status = True
 progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
 with open(filename, "wb") as f:
+    
     while True:
         # read 1024 bytes from the socket (receive)
         bytes_read = client_socket.recv(BUFFER_SIZE)
@@ -35,9 +36,15 @@ with open(filename, "wb") as f:
             # file transmitting is done
             break
         # write to the file the bytes we just received
+        status = False
         f.write(bytes_read)
         # update the progress bar
         progress.update(len(bytes_read))
+
+if status == True:
+    s.send(f"The program encountered an error transferring the file. Please try again.".encode())
+else:
+    s.send(f"Transfer of file {filename} was completed succesfully.".encode())
 
 # close the client socket
 client_socket.close()
