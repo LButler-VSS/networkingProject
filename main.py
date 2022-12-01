84391b29fbf3df0d9c2580c269a7e8041626acab
Logan Butler <logan3528@gmail.com>
2022-11-30 21:30:31 -0700

    threading addition

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import socket
 import os
 import tqdm
+import threading
 
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096
-
+#10.50.108.146
 host = "10.50.108.131"
 
 port = 5001
@@ -18,25 +19,33 @@ print(f"[+] Connecting to {host}:{port}")
 s.connect((host, port))
 print("[+] Connected.")
 
-# send the filename and filesize
-s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+def sendFile(filename, filesize, s):
+    # send the filename and filesize
+    s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
-progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename, "rb") as f:
-    while True:
-        # read the bytes from the file
-        bytes_read = f.read(BUFFER_SIZE)
-        if not bytes_read:
-            # file transmitting is done
-            break
-        # we use sendall to assure transimission in 
-        # busy networks
-        s.sendall(bytes_read)
-        # update the progress bar
-        progress.update(len(bytes_read))
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            s.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read  ))
 
+t = threading.Thread(target=sendFile, args=(filename, filesize, s))
+t.start()
+t.join()
 
-print(s.recv(BUFFER_SIZE).decode())
+# msg = s.recv(BUFFER_SIZE)
+# print(msg.decode)
+# while msg:
+#     print('Received: ' + msg.decode)
+#     msg = s.recv(BUFFER_SIZE)
 
 # close the socket
 s.close()
